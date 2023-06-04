e5d41bc38886fd6eaa63aef7c9fc1f08eca5143e
nmouatta <nargiss1685@instance-2.us-central1-b.c.mystical-rhino-388121.internal>
2023-06-04 17:27:20 +0000

    checkpoints path changed to required positional arg

--- predict.py
+++ predict.py
@@ -9,17 +9,17 @@ def main():
     data = generate_dataset(filenames, args.batch_size, args.input_img_shape)
 
     model = model_builder(args.input_img_shape)
-    model.load_weights(args.checkpoint_dir)
+    model.compile(metrics=['accuracy'])
+    model.load_weights(args.checkpoint_dir).expect_partial()
     eval_loss, eval_acc = model.evaluate(data)
     print('Eval loss: {}, Eval Accuracy: {}'.format(eval_loss, eval_acc))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('checkpoint_dir', type=str, help='Positional argument for the model weights checkpoint, such as: "./checkpoints/training/cp-000.ckpt". Make sure to not include ".data-00000-of-00001" in the path.')
     parser.add_argument('--data_dir', type=str, default='./data/cifar10/3.0.2/cifar10-test.tfrecord*',
                         help='Path to .tfrecord file, or file pattern for multiple files.')
-    parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints/training',
-                        help='Directory of training checkpoints')
     parser.add_argument('--input_img_shape', type=tuple, default=(32, 32, 3), help='Input img shape, ex: (32, 32, 3)')
     parser.add_argument('--batch_size', type=int, default=128, help='Global batch size')
     args = parser.parse_args()
