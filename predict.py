015c72b692d0c55bd2eaef11fb8a6e0b0c0dd5e5
Nargiss Mouatta <nargissmouatta@gmail.com>
2023-06-04 13:29:50 -0400

    formatting cleanup

--- predict.py
+++ predict.py
@@ -17,7 +17,9 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('checkpoint_dir', type=str, help='Positional argument for the model weights checkpoint, such as: "./checkpoints/training/cp-000.ckpt". Make sure to not include ".data-00000-of-00001" in the path.')
+    parser.add_argument('checkpoint_dir', type=str, help='Positional argument for the model weights checkpoint, '
+                                                         'such as: "./checkpoints/training/cp-000.ckpt". Make sure to'
+                                                         ' not include ".data-00000-of-00001" in the path.')
     parser.add_argument('--data_dir', type=str, default='./data/cifar10/3.0.2/cifar10-test.tfrecord*',
                         help='Path to .tfrecord file, or file pattern for multiple files.')
     parser.add_argument('--input_img_shape', type=tuple, default=(32, 32, 3), help='Input img shape, ex: (32, 32, 3)')
